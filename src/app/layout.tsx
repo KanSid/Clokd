9509704d6781044bc4444d254af464698d722d3e
KanSid <siddharthtreat@gmail.com>
2026-06-06 01:00:59 +0530

    speed_analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full">
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
